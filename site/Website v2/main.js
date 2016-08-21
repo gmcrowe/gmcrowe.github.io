de10257ef831989f519335af74163bfd684a2c9b
gmcrowe <gabriellemarie.crowe@gmail.com>
2016-08-21 13:43:35 -0400

    added site files
    site files

---
+++ site/Website v2/main.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function(){
+
+// ScrollTo Section
+$('#take-me-about').click(function(){
+  $.scrollTo('#all-about-me', 800);
+});
+
+$('#my-work-button').click(function(){
+  $.scrollTo('#my-work-section', 800);
+});
+
+$('#take-me-project').click(function(){
+  $.scrollTo('#my-work-section', 800);
+});
+
+$('#take-me-form').click(function(){
+  $.scrollTo('#see-my-form', 800);
+});
+
+//Parallax Effect
+$('.parallax-window').parallax({imageSrc: 'succulent.jpg'});
+
+});
