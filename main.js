584f916ac5adf93bee5f6accdd58926ceafd71a4
gmcrowe <gabriellemarie.crowe@gmail.com>
2016-08-28 12:47:00 -0400

    new
    new

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ var isiOS = /iPhone|iPad|iPod/i.test(navigator.userAgent)
 if (isiOS){
 	$(".parallax-window").css({
 		"background":"url('succulent@2x.png')",
-		// "background-size":"57px auto",
+		"background-size":"468px auto",
 		// "box-shadow": "inset 0 0 0 1000px rgba(255,255,255, 0.8)"
 		// "opacity":".8"
 	});
