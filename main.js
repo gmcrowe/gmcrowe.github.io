b8eb2e3a83bd0653bc551a7e68ce47f4d4c0a2f8
gmcrowe <gabriellemarie.crowe@gmail.com>
2016-08-28 13:04:42 -0400

    new
    new

--- main.js
+++ main.js
@@ -24,8 +24,7 @@ var isiOS = /iPhone|iPad|iPod/i.test(navigator.userAgent)
 if (isiOS){
 	$(".parallax-window").css({
 		"background":"url('succulent@2x.png')",
-		"-webkit-min-device-pixel-ratio":"2",
-		"background-position":"center top"
+		"-webkit-min-device-pixel-ratio":"1.5",
 	});
 } else {
 	$('.parallax-window').parallax({imageSrc:"succulent.jpg"});
