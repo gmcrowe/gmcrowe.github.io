df6b6a5a55e923111f12bad7e958a069f55f66f2
gmcrowe <gabriellemarie.crowe@gmail.com>
2016-09-03 22:06:45 -0400

    new
    new

--- main.js
+++ main.js
@@ -18,22 +18,22 @@ $('#take-me-form').click(function(){
   $.scrollTo('#see-my-form', 800);
 });
 
-// Parallax Effect
+// // Parallax Effect
 
-var isiOS = /iPhone|iPad|iPod/i.test(navigator.userAgent)
-if (isiOS){
-	$(".parallax-window").css({
-		"background":"url('succulent@2x.png')",
-		"background-position":"top center",
-	});
-} else {
-	$('.parallax-window').parallax({imageSrc:"succulent.jpg"});
+// var isiOS = /iPhone|iPad|iPod/i.test(navigator.userAgent)
+// if (isiOS){
+// 	$(".parallax-window").css({
+// 		"background":"url('succulent@2x.png')",
+// 		"background-position":"top center",
+// 	});
+// } else {
+// 	$('.parallax-window').parallax({imageSrc:"succulent.jpg"});
 
-	$(".parallax-slider").removeClass("hidden");
+// 	$(".parallax-slider").removeClass("hidden");
 
-	$(".parallax-window").attr("data-parallax", "scroll");
+// 	$(".parallax-window").attr("data-parallax", "scroll");
 
-	$(".parallax-window").attr("data-image-src", "succulent.jpg");
-}
+// 	$(".parallax-window").attr("data-image-src", "succulent.jpg");
+// }
 
 });
